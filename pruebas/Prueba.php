3a2b6f079fe999b2b83d20cc040bf2f2a00fb2b8
angelboxes <angel.cj.o@gmail.com>
2013-08-24 02:20:12 -0600

    ya no vamos a usar estos

---
+++ pruebas/Prueba.php
@@ -0,0 +1,14 @@
+<?php
+class Conexions PHPUnit_Framework_TestCase
+{
+  public function setUp(){ }
+  public function tearDown(){ }
+  public function ConexionIsValid()
+  {
+    // Prueba para probar que la conexión de la base de datos funcione 
+    $conn = mysql_connect("localhost","root" ,"");
+    $this->assertTrue($conn !== false);
+  }
+}	
+
+?>
